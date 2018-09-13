f0fb6e03f230f5a69486adde77b32f1b8c335304
zs <578069568@qq.com>
2018-09-13 16:21:00 +0800

    p2p项目第一天 完成项目环境的搭建和首页的制作

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+//引入gulp
+var gulp=require("gulp");
+
+//引入系统模块path路径
+var path=require("path");
+
+//引入插件模块
+var less=require("gulp-less");
+var sass=require("gulp-sass");
+var uglify=require("gulp-uglify");
+var rename=require("gulp-rename");
+var cleanCss=require("gulp-clean-css");
+
+//引入热刷新模块
+var livereload=require("gulp-livereload");
+
+//配置less任务
+gulp.task("lessTask",function () {
+    gulp.src('./src/less/*.less') 
+    .pipe(less({
+        paths: [ path.join(__dirname, 'less', 'includes') ] 
+    }))
+    .pipe(cleanCss()) 
+    .pipe(rename(function (path) {
+        path.basename += ".min";
+    })) 
+    .pipe(gulp.dest('./dist/css/minCss')) 
+    .pipe(livereload()); 
+});
+
+//配置sass任务
+gulp.task("sassTask",function () {
+    gulp.src('./src/scss/*.scss')
+    .pipe(sass().on('error', sass.logError)) 
+    .pipe(cleanCss()) 
+    .pipe(rename(function (path) {
+        path.basename += ".min"; 
+    })) 
+    .pipe(gulp.dest('./dist/css/minCss')) 
+    .pipe(livereload()); 
+});
+
+//配置js压缩任务
+gulp.task("uglifyJS",function () {
+    gulp.src('./src/js/*.js') 
+        .pipe(uglify()) 
+        .pipe(rename(function (path) {
+            path.basename += ".min"; 
+          })) 
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(livereload()); 
+});
+
+//配置default默认任务
+gulp.task("default",function () {
+    livereload.listen();
+    gulp.watch("./src/less/*.less",["lessTask"]);
+    gulp.watch("./src/scss/*.scss",["sassTask"]);
+    gulp.watch("./src/js/*.js",["uglifyJS"]);
+});
---
+++ register.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+<!-- 引入bootstrap -->
+<link rel="stylesheet" href="./lib/bootstrap/css/bootstrap.min.css">
+ <!-- 引入编译和压缩后的css文件 -->
+ <link rel="stylesheet" href="./dist/css/minCss/reglogin.min.css">
+</head>
+<body>
+
+
+
+
+
+<!-- 引入尾部信息 -->
+<?php require_once("footer.php");
+?>
+<!-- 引入jquery -->
+<script src="./lib/jquery/jquery.min.js"></script>
+<!-- 引入bootstrap.js -->
+<script src="./lib/bootstrap/js/bootstrap.min.js"></script>
+<!-- 引入自定义js -->
+<script src="./dist/js/index.min.js"></script>
+</body>
+</html>
